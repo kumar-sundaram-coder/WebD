60f9670ebb60f369f25a78c425001067ed678a39
Kumar Sundaram <sandyroshan2000@gmail.com>
2021-03-27 15:49:43 +0530

    .

--- Portfolio_Website/dist/js/typewriter.js
+++ Portfolio_Website/dist/js/typewriter.js
@@ -45,7 +45,7 @@ class TypeWriter {
       // Move to next word
       this.wordIndex++;
       // Pause before start typing
-      typeSpeed = 500;
+      typeSpeed = 300;
     }
 
     setTimeout(() => this.type(), typeSpeed);
